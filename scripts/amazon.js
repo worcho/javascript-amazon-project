a7bb3d0dc6c5f1c91186836261190e573fbb70d8
dvirisha <worcho95@gmail.com>
2024-11-30 13:03:32 +0200

    Added Util and checkout delete implementation

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,5 +1,6 @@
 import {cart, addToCart} from "../data/cart.js";
 import {products} from "../data/products.js";
+import { formatCurrency } from "./utils/money.js";
 
 let productsHtml = '';
 
@@ -24,7 +25,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-price">
-            ${(product.priceCents / 100).toFixed(2)}
+            ${formatCurrency(product.priceCents)}
           </div>
 
           <div class="product-quantity-container">
@@ -70,5 +71,6 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
   button.addEventListener('click', () => {
     addToCart(button.dataset.productId);
     updateCartQuantity();
+    console.log(cart);
   })
 })
